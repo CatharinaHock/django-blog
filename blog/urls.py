2aab8a4833bedc4a1c3c6d708889d20e752a22cc
chock <catharina_lan.hock@gmx.de>
2022-09-02 20:23:20 +0200

    added Login and prevented editing and creation of posts for unauthenticated users

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 from . import views
 
 urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("post/<int:pk>/", views.post_detail, name="post_detail"),
     path('post/new/', views.post_new, name='post_new'),
-      path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name="login"),
+    #path('', include('blog.urls')),
+    path("admin/", admin.site.urls),
 ]   

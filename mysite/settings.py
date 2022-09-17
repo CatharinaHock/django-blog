f5f4e9d8a71ca6cb66d2c7a4ae4186d1d83f19d4
chock <catharina_lan.hock@gmx.de>
2022-09-17 19:50:52 +0200

    Fixed django secret_key leak

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wl9_kjvj_*1+^zvmj9vu+^^(j0a+1vz4#(0e_nlz7fww@9^&w-'
+SECRET_KEY = Config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
